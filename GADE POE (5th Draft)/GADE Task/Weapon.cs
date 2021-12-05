8da79333de39c8c336cf4721ad9a0a66ec369abb
HoziKwitty <88772451+HoziKwitty@users.noreply.github.com>
2021-12-05 22:27:53 +0200

    Add files via upload

---
+++ GADE POE (5th Draft)/GADE Task/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GADE_Task
+{
+    public abstract class Weapon : Item
+    {
+        protected int damage;
+        public int GetDamage { get { return damage; } set { damage = value; } }
+
+        protected int range;
+        public virtual int GetRange { get { return range; } set { range = value; } }
+
+        protected int durability;
+        public int GetDurability { get { return durability; } set { durability = value; } }
+
+        protected int cost;
+        public int GetCost { get { return cost; } set { cost = value; } }
+
+        protected string type;
+        public string GetType { get { return type; } set { type = value; } }
+
+        public Weapon(int inX, int inY) : base(inX, inY, GameEngine.GetWeaponSymbol)
+        {
+
+        }
+
+
+    }
+}
